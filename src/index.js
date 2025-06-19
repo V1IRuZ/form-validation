055927987142017603cf626208046656e0133987
Esa <esa-pitkanen@hotmail.com>
2025-06-19 18:20:12 +0300

    Add validation checks
    The email is checked to ensure that it meets the input type requirements.
    
    The country must be a minimum of 4 characters and a maximum of 56 characters.
    
    The postal code has a specific pattern that accepts certain characters.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-import "./styles.css"
+import "./styles.css";
+
+const email = document.querySelector(".email");
+const emailInput = document.querySelector("#mail");
+const emailError = document.querySelector(".email > .error");
+
+emailInput.addEventListener("input", () => {
+  if (!emailInput.validity.valid) {
+    showEmailError();
+  } else {
+    emailError.textContent = "";
+  }
+});
+
+function showEmailError() {
+  if (emailInput.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (emailInput.validity.typeMismatch) {
+    emailError.textContent = "That's not real email address.";
+  }
+}
+
+const country = document.querySelector(".country");
+const countryInput = document.querySelector("#country");
+const countryError = document.querySelector(".country > .error");
+
+countryInput.addEventListener("input", () => {
+  if (!countryInput.validity.valid) {
+    showCountryError();
+  } else {
+    countryError.textContent = "";
+  }
+});
+
+function showCountryError() {
+  if (countryInput.validity.valueMissing) {
+    countryError.textContent = "You need to enter a country";
+  } else if (countryInput.validity.tooShort) {
+    countryError.textContent = "Every country name has at least four letters.";
+  } else if (countryInput.validity.tooLong) {
+    countryError.textContent =
+      "The longest country name has only 56 characters.";
+  }
+}
+
+const postalCodeInput = document.querySelector("#postal-code");
+const postalCodeError = document.querySelector(".postal-code > .error");
+
+postalCodeInput.addEventListener("input", () => {
+    if (!postalCodeInput.validity.valid) {
+        showPostalCodeError()
+    } else {
+        postalCodeError.textContent = "";
+    }
+})
+
+function showPostalCodeError() {
+    if (postalCodeInput.validity.valueMissing) {
+        postalCodeError.textContent = "You need to give your postal code";
+    } else if (postalCodeInput.validity.patternMismatch) {
+        postalCodeError.textContent = "That postal code does not exist."
+    }
+}
